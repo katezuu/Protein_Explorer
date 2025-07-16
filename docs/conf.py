5d429b2f693b25bdb1e7b2793b011ed63e6bc895
katezuu <Paramonova_Kat@mail.ru>
2025-07-16 14:12:10 +0300

    github pages

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Protein Structure Explorer'
+author = 'Ekaterina Paramonova'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # для Google/NumPy docstrings
+    'myst_parser',          # если вы хотите Markdown
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# HTML output
+html_theme = 'furo'
+html_static_path = ['_static']
