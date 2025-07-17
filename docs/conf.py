97a7dddd36f943218854ec13207c6b4b40cefeee
katezuu <Paramonova_Kat@mail.ru>
2025-07-17 17:37:24 +0300

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,13 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'Protein Structure Explorer'
-author  = 'Ekaterina Paramonova'
+author = 'Ekaterina Paramonova'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
